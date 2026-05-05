2032e60613feb26a3e2dc7765fa062e6a300bfd6
ankitojha02 <ojhaa2207@gmail.com>
2026-05-05 12:24:39 +0530

    creating user schema

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import authRoutes from "./routes/authRoutes.js";
 import cors from "cors";
 
 const app = express();
@@ -9,5 +10,7 @@ app.use(express.json());
 app.get("/health", (req, res) => {
   res.send("DevConnect API Running 🚀");
 });
+
+app.use("/api/auth", authRoutes);
 
 export default app;
---
+++ server/src/controllers/authController.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from "express";
+import { User } from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const signup = async (req: Request, res: Response) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({ message: "User created", user });
+  } catch (error) {
+    res.status(500).json({ message: "Signup failed", error });
+  }
+};
+
+export const login = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { userId: user._id },
+      process.env.JWT_SECRET as string,
+      { expiresIn: "7d" }
+    );
+
+    res.json({ message: "Login successful", token });
+  } catch (error) {
+    res.status(500).json({ message: "Login failed", error });
+  }
+};
